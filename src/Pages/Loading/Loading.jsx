2d27f2060b54401f9b19e112d347b0ed5c647af8
ahmedsami019 <easami019192@gmail.com>
2026-06-13 03:05:51 +0600

    created loading page

--- src/Pages/Loading/Loading.jsx
+++ src/Pages/Loading/Loading.jsx
@@ -1,7 +1,11 @@
+import LottieModule from "lottie-react";
+import loading from "../../assets/Animations/loading.json"
+const Lottie = LottieModule.default || LottieModule;
+
 const Loading = () => {
   return (
     <div className="flex items-center justify-center mt-30">
-      <span className="loading loading-ring loading-xl"></span>
+      <Lottie animationData={loading} loop={true}/>
     </div>
   );
 };
